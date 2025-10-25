e78d61063734ec9b2ddf001e4723fb0975372f1b
Arjan Anant <arjananant2@gmail.com>
2025-10-25 16:44:09 +0000

    same error is poping up again

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,3 +8,17 @@ export const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
+if (!firebaseConfig.apiKey || !firebaseConfig.projectId) {
+  console.warn(`
+    Firebase configuration is missing.
+    Please make sure you have a .env file with the following content:
+
+    NEXT_PUBLIC_FIREBASE_API_KEY="your-api-key"
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN="your-auth-domain"
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID="your-project-id"
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET="your-storage-bucket"
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID="your-messaging-sender-id"
+    NEXT_PUBLIC_FIREBASE_APP_ID="your-app-id"
+  `);
+}
