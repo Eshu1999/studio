684562816ae342c729156c9cd78ec58a52ccfeeb
Arjan Anant <arjananant2@gmail.com>
2025-11-02 16:31:10 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-7418008653-c4745",
+  "projectId": "faylocare",
   "appId": "1:60390087515:web:6ac6c8e5839dc8e77d187e",
   "apiKey": "AIzaSyDVOmrQV3rdDzYMm3aw06TNCuwJPVwlDMU",
-  "authDomain": "studio-7418008653-c4745.firebaseapp.com",
-  "storageBucket": "studio-7418008653-c4745.appspot.com",
+  "authDomain": "faylocare.firebaseapp.com",
+  "storageBucket": "faylocare.appspot.com",
   "measurementId": "",
   "messagingSenderId": "60390087515"
 };
